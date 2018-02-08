7757ae5c67190eb49e06ca522d0f29411c8bad12
Jenna Stephens <jsteph11@127x36.csc.calpoly.edu>
2018-02-07 17:59:02 -0800

    continued work on fib

--- lab4/fib.asm
+++ lab4/fib.asm
@@ -1,21 +1,75 @@
 #fibonacci value function
+.data
 
 var1:	.word	0
 var2:	.word	1
 var3:	.word	5
 var4:	.word	10
 var5:	.word	20
+result:	.word	0
 
-main:	
+.text
+
+
+main:
+	# Solve for var1
+	lw 	$a1, var3	#solve for var1
+	jal	fib		#jump to fibonacci function
+	
+	#Display var1 Result
+	sw	$v1, result
+	la	$a0, result	#loads address of result into $a0
+	li	$v0,	4	#sets syscall to print_string service
+	syscall			#prints out string in $a0
+	
+	#Exit
+	li	$v0, 	10	#exit
+	syscall
 
 
 #------------------------------------------------------------
 #-
 #- 	Parameters: 
-#-		- $a1
+#-		- $a1: variable we want to solve for
+#-		- $v1: answer to fibonacci sequence
 #-
-#-	Variable:
+#-	Variables:
+#-		- $v1: result
+#-		- $ra: return address
+#-		- $sp: stack pointer
+#-		- $s0: sum
 #-
 #----------------------------------------------------------
 
 fib:	
+	#store local variables on stack
+	add	$sp, $sp, -12	#space for two values
+	sw	$ra, 0($sp)	#store return address
+	sw	$s0, 4($sp)	#store local variable
+	sw	$s1, 8($sp)
+	
+	#Base Case
+	li $v1 1
+	beq $a0, 0, endFib 	#argument = 0
+	beq $a0, 1, endFib 	#argument = 1
+	
+	#fib(n-1)
+	add $a0, $a0, -1
+	move $s0, $a0
+	jal fib
+	
+	#fib(n-2)
+	add $a0, $a0, -1
+	move $s0, $a0
+	jal fib
+	
+	#Sum
+	add $v1, $s0, $v1
+	add $v1, $s1, $v1
+	
+	endFib:
+		lw 	$ra, ($sp) 	#load return address from stack
+		lw 	$s0, 4($sp)	#load local variable from stack
+		lw	$s1, 8($sp)
+		add	$sp, $sp, 12	#restore stack
+		jr	$ra		#return
