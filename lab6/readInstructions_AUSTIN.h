f810cd00477077d35630cf71ae310e6669f59ed2
Austin Whaley <austin.whaley654@gmail.com>
2018-02-26 15:32:00 -0800

    "newest"

--- lab6/readInstructions_AUSTIN.h
+++ lab6/readInstructions_AUSTIN.h
@@ -10,13 +10,13 @@ typedef unsigned int MIPS, *MIPS_PTR;
 /*----------Prototypes---------*/
 int func_code(MIPS ir); 
 unsigned int op_code(MIPS ir); 
-void shamt(MIPS ir); 
+unsigned int shamt(MIPS ir); 
 int imm_val(MIPS ir);
-void eff_addr(int pc, int imm_value);
+int eff_addr(int pc, int imm_value);
 char type(unsigned int op_code, MIPS ir);
-void jmp_addr(MIPS ir);
-void printReg(MIPS ir, int rs_en, int rt_en, int rd_en);
-void eff_addr_ls(MIPS ir);
+unsigned int jmp_addr(MIPS ir);
+void  printReg(MIPS ir, int rs_en, int rt_en, int rd_en);
+unsigned int eff_addr_ls(MIPS ir);
 void printRegRS(MIPS ir);
 void readInstructions(int memp);
 
