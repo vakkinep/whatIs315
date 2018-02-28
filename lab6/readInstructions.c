c5b2c089c4f61ffb24deeb1e876fbc1281663c0d
Austin Whaley <austin.whaley654@gmail.com>
2018-02-28 15:40:23 -0800

    "included single step and removed _AUSTIN"

--- lab6/readInstructions.c
+++ lab6/readInstructions.c
@@ -20,7 +20,7 @@ typedef struct INSTRUCTION {
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "readInstructions_AUSTIN.h"
+#include "readInstructions.h"
 #include "startChecks.h"
 #include "mips_asm_header.h"
 
