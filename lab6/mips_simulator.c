7146c417544050cf55c11f58dec0c45e45364f4c
Austin Whaley <austin.whaley654@gmail.com>
2018-02-28 18:16:26 -0800

    "added execute function and trimmed func_code"

--- lab6/mips_simulator.c
+++ lab6/mips_simulator.c
@@ -7,22 +7,22 @@
 MIPS mem[1024];	
 
 int runSimulator(char mode, int memp, REG regs[], int pc) {
-	INST instruct;
+   INST instruct;
    if (mode == 's') {
       //run one instruction and print
-   		if (pc>=memp) {
-   			printf("Done\n");
-   			exit(0);
-   		}
-   	  else {
-   	  	pc = readNextInst(memp, regs, pc, instruct);
-   	  	printValues(instruct);
-   	  }
+      if (pc>=memp) {
+         printf("Done\n");
+         exit(0);
+      }
+      else {
+         pc = readNextInst(memp, regs, pc, instruct);
+         printValues(instruct);
+      }
    } else if (mode == 'r') {
       //run all instructions and print only end result
-   	  pc = readInstructions(memp, regs, pc, instruct);
-   	   printValues(instruct);
-   	   exit(0);
+      pc = readInstructions(memp, regs, pc, instruct);
+      printValues(instruct);
+      exit(0);
    }
    else {
       printf("ERROR - USAGE \n\t 'r' - run \n\t 's' - single step\n\n");
@@ -31,55 +31,55 @@ int runSimulator(char mode, int memp, REG regs[], int pc) {
 }
 
 /*==================================TO DO======================================
-*     1.Print statistics
-*        a. Clock cycles 
-*        b. Number of memory references (read/writes)
-*        c. Number of instructions executed
-*        d. Print contents of the registers
-*     2.Simulate memory
-*        a. Init 32 registers to 0 (unigned int array reg[32])
-*        b. Create "base" of 0x400000
-*        c. Create an offset for fed in pointers
-*     3.Print results of the run
-*     4.End if syscall with function code of 10 in $v0
-*     5.Take input for single step and wait for either "run" to change mode or 
-*        input for next step.
-*     6.
-*
-*
-*
-*=============================================================================*/
+ *     1.Print statistics
+ *        a. Clock cycles 
+ *        b. Number of memory references (read/writes)
+ *        c. Number of instructions executed
+ *        d. Print contents of the registers
+ *     2.Simulate memory
+ *        a. Init 32 registers to 0 (unigned int array reg[32])
+ *        b. Create "base" of 0x400000
+ *        c. Create an offset for fed in pointers
+ *     3.Print results of the run
+ *     4.End if syscall with function code of 10 in $v0
+ *     5.Take input for single step and wait for either "run" to change mode or 
+ *        input for next step.
+ *     6.
+ *
+ *
+ *
+ *=============================================================================*/
 
 
 /* Functions: 
-*     PrintRegisters(); //Prints out the registers and their contents, useful for single step and end of run
-*     AccessMemory();   //Uses the memory offset to access the memory heap and write or read
-*     
-*     
-*     
-*     
-*     
-*/
+ *     PrintRegisters(); //Prints out the registers and their contents, useful for single step and end of run
+ *     AccessMemory();   //Uses the memory offset to access the memory heap and write or read
+ *     
+ *     
+ *     
+ *     
+ *     
+ */
 int main(int argc, char *argv[]) {
-	char ch;
-	int memp;                                                                     //Size of pulled instructions
-   	int pc = 0;
-   	REG regs[32] = {0};
+   char ch;
+   int memp;                                                                     //Size of pulled instructions
+   int pc = 0;
+   REG regs[32] = {0};
 
-   	checkInputs(argc, argv);
-   	memp = checkFile(argv, mem, sizeof(mem));
+   checkInputs(argc, argv);
+   memp = checkFile(argv, mem, sizeof(mem));
 
-	//printf("\nEnter mode ('r' - run, 's' - single step): \n");
-	while ( 1 ) {
-		char t;
-		printf("\nChoose Mode ('r' - run, 's' - single step, 'x' - exit): ");
-		ch=getchar();
-	
-		if (ch == 'x') {
-			printf("Exiting.\n");
-			break;
-		}
-		pc = runSimulator(ch, memp, regs, pc);
-		while ((t = getchar()) != EOF && t != '\n'); 								//loop through rest of stdin
-	}
+   //printf("\nEnter mode ('r' - run, 's' - single step): \n");
+   while ( 1 ) {
+      char t;
+      printf("\nChoose Mode ('r' - run, 's' - single step, 'x' - exit): ");
+      ch=getchar();
+
+      if (ch == 'x') {
+         printf("Exiting.\n");
+         break;
+      }
+      pc = runSimulator(ch, memp, regs, pc);
+      while ((t = getchar()) != EOF && t != '\n'); 								//loop through rest of stdin
+   }
 }
--- lab6/readInstructions.c
+++ lab6/readInstructions.c
@@ -18,72 +18,74 @@ MIPS mem[1024];	                                                       	        
 
 //============PROTOTYPE
 void printValues(INST instruction);
+void execute(INST instruction, int ir);
 
 
 /*int main(int argc, char *argv[]) {
-   int memp;                                                                     //Size of pulled instructions
-   REG regs[32] = {0};
+  int memp;                                                                     //Size of pulled instructions
+  REG regs[32] = {0};
 
-   checkInputs(argc, argv);
-   memp = checkFile(argv, mem, sizeof(mem));
-   readInstructions(memp, regs);
-   printValues(instruct);
-}*/
+  checkInputs(argc, argv);
+  memp = checkFile(argv, mem, sizeof(mem));
+  readInstructions(memp, regs);
+  printValues(instruct);
+  }*/
 
 int readInstructions(int memp, REG regs[], int pc, INST instruct) {
-	while( pc<memp ) {	                                                 /* i contains byte offset addresses */
-    	pc = readNextInst(memp, regs, pc, instruct);								//increment pc inside for jump and branch
-  	}
-  	printf("\n\n");
-  	return pc;
+   while( pc<memp ) {	                                                 /* i contains byte offset addresses */
+      pc = readNextInst(memp, regs, pc, instruct);								//increment pc inside for jump and branch
+   }
+   printf("\n\n");
+   return pc;
 }
 
 int readNextInst(int memp, REG regs[], int pc, INST instruct) {
-	int curr_instruction = mem[pc/4];
-	printf("\n@PC=%08X, ", pc);
+   int curr_instruction = mem[pc/4];
+   printf("\n@PC=%08X, ", pc);
 
-      instruct.opcode = op_code(curr_instruction);		                         //Print Opcode
-      instruct.type = type(instruct.opcode, curr_instruction);			         //Print instruction type
+   instruct.opcode = op_code(curr_instruction);		                         //Print Opcode
+   instruct.type = type(instruct.opcode, curr_instruction);			         //Print instruction type
 
-      //if R-Type
-      if (instruct.type == 'r') { 
-      	instruct.func_code = func_code(curr_instruction);
-         if (instruct.func_code) {			                 					//print function code
-         	instruct.rs = reg_s(curr_instruction);
-         	instruct.rt = reg_t(curr_instruction);
-         	instruct.rd = reg_d(curr_instruction);
-            instruct.shamt = shamt(curr_instruction); 			                         //print shift amount
-        }
-    }
+   //if R-Type
+   if (instruct.type == 'r') { 
+      instruct.func_code = func_code(curr_instruction);
+      if (instruct.func_code) {			                 					//print function code
+         instruct.rs = reg_s(curr_instruction);
+         instruct.rt = reg_t(curr_instruction);
+         instruct.rd = reg_d(curr_instruction);
+         instruct.shamt = shamt(curr_instruction); 			                         //print shift amount
+         execute(instruct, curr_instruction);
+      }
+   }
 
-      //if I-Type, print immediate value
-    if (instruct.type == 'i'|| instruct.type == 'b' || instruct.type == 's') {
-    	instruct.rs = reg_s(curr_instruction);
-    	instruct.rt = reg_t(curr_instruction);
-    	instruct.immed = imm_val(curr_instruction);
-    }
+   //if I-Type, print immediate value
+   if (instruct.type == 'i'|| instruct.type == 'b' || instruct.type == 's') {
+      instruct.rs = reg_s(curr_instruction);
+      instruct.rt = reg_t(curr_instruction);
+      instruct.immed = imm_val(curr_instruction);
+   }
 
-      //if I-Type (Branch), print branch address
-    if (instruct.type == 'b') {
-    	regs[31] = pc + 4;
-    	return (pc = eff_addr(pc, instruct.immed));
-    }
+   //if I-Type (Branch), print branch address
+   if (instruct.type == 'b') {
+      regs[31] = pc + 4;
+      return (pc = eff_addr(pc, instruct.immed));
+   }
 
-      //if J-Type, print Jump Address
-    if (instruct.type == 'j') {
-    	instruct.jmp_addr = jmp_addr(curr_instruction);
-         if (instruct.opcode == 0x03) {												//jump and link
-         	regs[31] = pc + 4;
-         }
-         return (pc = instruct.jmp_addr); 
-     }
+   //if J-Type, print Jump Address
+   if (instruct.type == 'j') {
+      instruct.jmp_addr = jmp_addr(curr_instruction);
+      if (instruct.opcode == 0x03) {												//jump and link
+         regs[31] = pc + 4;
+      }
+      return (pc = instruct.jmp_addr); 
+   }
 
-      //if Store/load I-Type
-     if (instruct.type == 's') {
-     	eff_addr_ls(curr_instruction);
-     }     
-      return pc+4;																	//increment pc
-  }
+   //if Store/load I-Type
+   if (instruct.type == 's') {
+      eff_addr_ls(curr_instruction);
+   }     
+   return pc+4;																	//increment pc
+}
 
 
 /************************************************************************************************
@@ -91,93 +93,94 @@ int readNextInst(int memp, REG regs[], int pc, INST instruct) {
 
  ************************************************************************************************/
 
-  unsigned int op_code(MIPS ir) {
-  	unsigned int op_code;
+unsigned int op_code(MIPS ir) {
+   unsigned int op_code;
 
    op_code = ((ir >> 26) & 0x00000003F);                                         //Mask all but opcode
    return op_code;
 }
 
 int imm_val(MIPS ir) {                                                           //I Types
-	unsigned int imm_val;
-	unsigned int sign_ext;
-	int sign;
+   unsigned int imm_val;
+   unsigned int sign_ext;
+   int sign;
 
-	imm_val = (ir & 0x0000FFFF); 
-	printf("Imm=0x%04X, ", imm_val);
+   imm_val = (ir & 0x0000FFFF); 
+   printf("Imm=0x%04X, ", imm_val);
 
-	sign = (imm_val & 0x00008000);
-	if (sign) {
-		sign_ext = (imm_val | 0xFFFF0000);
-	}
-	printf("signext: 0x%08X (%d)", sign_ext, (int) sign_ext);
-	return imm_val;
+   sign = (imm_val & 0x00008000);
+   if (sign) {
+      sign_ext = (imm_val | 0xFFFF0000);
+   }
+   printf("signext: 0x%08X (%d)", sign_ext, (int) sign_ext);
+   return imm_val;
 }
 
 unsigned int shamt(MIPS ir) {                                                            //R Types
-	unsigned int shamt;
+   unsigned int shamt;
 
-	shamt = ((ir >> 6) & 0x00000001F);    
-	return shamt;
+   shamt = ((ir >> 6) & 0x00000001F);    
+   return shamt;
 }
 
 int func_code(MIPS ir) {                                                         //R Types
-	unsigned int func_code;
+   unsigned int func_code;
 
-	func_code = (ir & 0x0000003F);
+   func_code = (ir & 0x0000003F);
+   return func_code;
+}
 
-	switch (func_code) {
-		case 0x20: printf("R Type, Function=0x%02X (add)\n", func_code); break;
-		case 0x21: printf("R Type, Function=0x%02X (addu)\n", func_code); break;
-		case 0x22: printf("R Type, Function=0x%02X (sub)\n", func_code); break;
-		case 0x23: printf("R Type, Function=0x%02X (subu)\n", func_code); break;
-		case 0x24: printf("R Type, Function=0x%02X (and)\n", func_code); break;
-		case 0x27: printf("R Type, Function=0x%02X (nor)\n", func_code); break;
-		case 0x25: printf("R Type, Function=0x%02X (or)\n", func_code); break;
-		case 0x26: printf("R Type, Function=0x%02X (xor)\n", func_code); break;
-		case 0x00: printf("R Type, Function=0x%02X (sll)\n", func_code); break;
-		case 0x02: printf("R Type, Function=0x%02X (srl)\n", func_code); break;
-		case 0x03: printf("R Type, Function=0x%02X (sra)\n", func_code); break;
-		case 0x04: printf("R Type, Function=0x%02X (sllv)\n", func_code); break;
-		case 0x06: printf("R Type, Function=0x%02X (srlv)\n", func_code); break;
-		case 0x07: printf("R Type, Function=0x%02X (srav)\n", func_code); break;
-		case 0x2A: printf("R Type, Function=0x%02X (slt)\n", func_code); break;
-		case 0x2B: printf("R Type, Function=0x%02X (sltu)\n", func_code); break;
-		case 0x08: printf("R Type, Function=0x%02X (jr)\n", func_code); break;
-		case 0x09: printf("R Type, Function=0x%02X (jalr)\n", func_code); break;
-		case 0x1A: printf("R Type, Function=0x%02X (div)\n", func_code); break;
-		case 0x1B: printf("R Type, Function=0x%02X (divu)\n", func_code); break;
-		case 0x18: printf("R Type, Function=0x%02X (mult)\n", func_code); break;
-		case 0x19: printf("R Type, Function=0x%02X (multu)\n", func_code); break;
-		default:
-		printf("0x%08X - Invalid Instruction.", ir);
-		return 0;
-	}
-	return 1;
+void execute(INST instruct, int ir) {
+   switch (instruct.func_code) {
+      case 0x20: printf("R Type, Function=0x%02X (add)\n", instruct.func_code); break;
+      case 0x21: printf("R Type, Function=0x%02X (addu)\n", instruct.func_code); break;
+      case 0x22: printf("R Type, Function=0x%02X (sub)\n", instruct.func_code); break;
+      case 0x23: printf("R Type, Function=0x%02X (subu)\n", instruct.func_code); break;
+      case 0x24: printf("R Type, Function=0x%02X (and)\n", instruct.func_code); break;
+      case 0x27: printf("R Type, Function=0x%02X (nor)\n", instruct.func_code); break;
+      case 0x25: printf("R Type, Function=0x%02X (or)\n", instruct.func_code); break;
+      case 0x26: printf("R Type, Function=0x%02X (xor)\n", instruct.func_code); break;
+      case 0x00: printf("R Type, Function=0x%02X (sll)\n", instruct.func_code); break;
+      case 0x02: printf("R Type, Function=0x%02X (srl)\n", instruct.func_code); break;
+      case 0x03: printf("R Type, Function=0x%02X (sra)\n", instruct.func_code); break;
+      case 0x04: printf("R Type, Function=0x%02X (sllv)\n", instruct.func_code); break;
+      case 0x06: printf("R Type, Function=0x%02X (srlv)\n", instruct.func_code); break;
+      case 0x07: printf("R Type, Function=0x%02X (srav)\n", instruct.func_code); break;
+      case 0x2A: printf("R Type, Function=0x%02X (slt)\n", instruct.func_code); break;
+      case 0x2B: printf("R Type, Function=0x%02X (sltu)\n", instruct.func_code); break;
+      case 0x08: printf("R Type, Function=0x%02X (jr)\n", instruct.func_code); break;
+      case 0x09: printf("R Type, Function=0x%02X (jalr)\n", instruct.func_code); break;
+      case 0x1A: printf("R Type, Function=0x%02X (div)\n", instruct.func_code); break;
+      case 0x1B: printf("R Type, Function=0x%02X (divu)\n", instruct.func_code); break;
+      case 0x18: printf("R Type, Function=0x%02X (mult)\n", instruct.func_code); break;
+      case 0x19: printf("R Type, Function=0x%02X (multu)\n", instruct.func_code); break;
+      default:
+                 printf("0x%08X - Invalid Instruction.", ir);
+   }
 }
 
 int eff_addr(int pc, int imm_value) {                                           //I Types, Branches only
-	int eff_addr;
+   int eff_addr;
 
-	eff_addr = pc + imm_value;
-	return eff_addr;
+   eff_addr = pc + imm_value;
+   return eff_addr;
 }
 
 unsigned int jmp_addr(MIPS ir) {                                                         //J Types
-	unsigned int jmp_addr;
+   unsigned int jmp_addr;
 
-	jmp_addr = (ir & 0x03FFFFFF);
+   jmp_addr = (ir & 0x03FFFFFF);
    //printf("addr=0x%06X \n", jmp_addr);
 
-	jmp_addr <<= 2;	
-	return jmp_addr;
+   jmp_addr <<= 2;	
+   return jmp_addr;
 }
 
 char type(unsigned int op_code, MIPS ir) {
-	if (ir == 0) {
-		return 'n';
-	}
-	switch(op_code) {
+   if (ir == 0) {
+      return 'n';
+   }
+   switch(op_code) {
       case 0x00: return 'r'; 
       case 0x02: printf("OpCode=0x%02X, J Type (j), ", op_code); return 'j'; 
       case 0x03: printf("OpCode=0x%02X, J Type (jal), ", op_code); return 'j';
@@ -204,64 +207,59 @@ char type(unsigned int op_code, MIPS ir) {
       case 0x39: printf("OpCode=0x%02X, I Type (swcl)\n", op_code); return 's';
       case 0x3D: printf("OpCode=0x%02X, I Type (sdcl)\n", op_code); return 's';
       default:
-      printf("0x%08X - Invalid Instruction.\n", ir);
-      return '0';
-  }
+                 printf("0x%08X - Invalid Instruction.\n", ir);
+                 return '0';
+   }
 }
 
 
 unsigned int eff_addr_ls(MIPS ir) {
-	unsigned int ext_imm = (ir & 0x0000FFFF);
-	ext_imm |= ((ext_imm & 0x8000) ? 0xFFFF0000 : 0);
-   //printf(", \nEffAddr=R[");
-	reg_s(ir);
-   //printf("] + 0x%08X", ext_imm);
-	return ext_imm;
+   unsigned int ext_imm = (ir & 0x0000FFFF);
+   ext_imm |= ((ext_imm & 0x8000) ? 0xFFFF0000 : 0);
+   reg_s(ir);
+   return ext_imm;
 }
 
 int reg_s(MIPS ir) {
-	int rs = (ir >> 21) & 0x1F;
-	//printReg(rs, "rs");
-	return rs;
+   int rs = (ir >> 21) & 0x1F;
+   return rs;
 }
 
 int reg_t(MIPS ir) {
-	int rt = (ir >> 16) & 0x1F;
-	//printReg(rt, "rt");
-	return rt;
+   int rt = (ir >> 16) & 0x1F;
+   return rt;
 }
 
 int reg_d(MIPS ir) {
-	int rd = (ir >> 11) & 0x1F;
-	//printReg(rd, "rd");
-	return rd;
+   int rd = (ir >> 11) & 0x1F;
+   return rd;
 }
 
 void printReg(int reg, char* title) { //title will be which register (rs, rt, rd)
-	int name;
-	if (reg == 0) {
-		printf("%s=%d ($zero), ", title, reg);
-	} else if (reg <= 3) {
-		name= reg - 2;
-		printf("%s=%d ($v%d), ", title, reg, name);
-	} else if (reg <= 7) {
-		name= reg - 4;
-		printf("%s=%d ($a%d), ", title, reg, name);
-	} else if (reg <= 15) {
-		name= reg - 8;
-		printf("%s=%d ($t%d), ", title, reg, name);
-	} else if (reg <= 23) {
-		name= reg - 16;
-		printf("%s=%d ($s%d), ", title, reg, name);
-	} else if (reg == 31) {
-		printf("%s=%d ($ra, ", title, reg);
-	}
+   int name;
+   if (reg == 0) {
+      printf("%s=%d ($zero), ", title, reg);
+   } else if (reg <= 3) {
+      name= reg - 2;
+      printf("%s=%d ($v%d), ", title, reg, name);
+   } else if (reg <= 7) {
+      name= reg - 4;
+      printf("%s=%d ($a%d), ", title, reg, name);
+   } else if (reg <= 15) {
+      name= reg - 8;
+      printf("%s=%d ($t%d), ", title, reg, name);
+   } else if (reg <= 23) {
+      name= reg - 16;
+      printf("%s=%d ($s%d), ", title, reg, name);
+   } else if (reg == 31) {
+      printf("%s=%d ($ra, ", title, reg);
+   }
 }
 
 void printValues(INST instruction) {                                             // Prints the instruction values
-	printf("OPCODE: 0x%06X\tTYPE: %c\n",instruction.opcode, instruction.type);
-	switch (instruction.type) {
-		case ('r') : 
+   printf("OPCODE: 0x%06X\tTYPE: %c\n",instruction.opcode, instruction.type);
+   switch (instruction.type) {
+      case ('r') : 
          printf("values: \n");                                                   //r-type       AUSTIN
          printReg(instruction.rs, "rs");
          printReg(instruction.rt, "rt");
@@ -270,13 +268,13 @@ void printValues(INST instruction) {                                            
          printf("SHAMT: %d\t FUNCTION: 0x%02X\n\n\n", instruction.shamt, instruction.func_code);
          break;
       case ('i') : break;                                                        //i-type       AUSTIN
-      printf("values: \n");
-      printReg(instruction.rs, "rs");
-      printReg(instruction.rt, "rt");
-      printf("Immed value: 0x%04X\n", instruction.immed);
+                   printf("values: \n");
+                   printReg(instruction.rs, "rs");
+                   printReg(instruction.rt, "rt");
+                   printf("Immed value: 0x%04X\n", instruction.immed);
       case ('b') : break;                                                        //brn inst     VINNIE
       case ('s') : break;                                                        //ld/store     VINNIE
       case ('j') : printf("jmp_addr=0x%06X\n", jmp_addr); break;                                                        //j-type
       case ('n') : break;                                                        //nop
-  }
+   }
 }
